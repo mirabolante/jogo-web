12ef9efaf101bb74b0a754275aadc7e8ad3996a2
juliasilveira <julia.ss08@aluno.ifsc.edu.br>
2021-02-28 22:25:27 +0000

    entrega 05 (atualizar)

--- cliente/js/cena0.js
+++ cliente/js/cena0.js
@@ -9,23 +9,8 @@ cena0.preload = function () {
     this.load.image("cena0", "assets/cena0.png");
 };
 
+// Criar botão com a imagem de fundo
 cena0.create = function () {
-    // Conectar no servidor via WebSocket
-    this.socket = io();
-
-    // Disparar evento quando jogador entrar na partida
-    var self = this;
-    this.socket.on("jogadorEntrou", function (player) {
-        console.log("Meu id: %s", self.socket.id);
-        console.log("Jogador %s entrou na partida", player);
-    });
-
-    // Disparar evento quando jogador sair da partida
-    this.socket.on("disconnect", function (player) {
-        console.log("Jogador %s saiu na partida", player);
-    });
-
-    // Botão com a imagem de fundo
     var button = this.add.image(400, 300, "cena0", 0).setInteractive();
 
     // Ao clicar no botão, inicia a cena 1
